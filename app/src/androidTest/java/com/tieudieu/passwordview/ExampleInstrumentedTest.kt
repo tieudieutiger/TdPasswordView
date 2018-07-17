264601136ecdaa886d310e056074d15d1d336626
chienchieu <vuvantrieu.k54.cb@gmail.com>
2018-07-17 14:18:32 +0700

    Init app

---
+++ app/src/androidTest/java/com/tieudieu/passwordview/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.tieudieu.passwordview
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.tieudieu.passwordview", appContext.packageName)
+    }
+}
